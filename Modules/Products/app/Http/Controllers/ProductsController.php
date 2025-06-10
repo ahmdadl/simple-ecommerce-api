286efebc2c3715b73a18863b76a77b3425e3b774
abo3adel <abo3adel35@gmail.com>
2025-06-10 21:33:23 +0300

    feat(products): get current parent record

--- Modules/Products/app/Http/Controllers/ProductsController.php
+++ Modules/Products/app/Http/Controllers/ProductsController.php
@@ -3,8 +3,13 @@
 namespace Modules\Products\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Modules\Brands\Models\Brand;
+use Modules\Brands\Transformers\BrandResource;
+use Modules\Categories\Models\Category;
+use Modules\Categories\Transformers\CategoryResource;
 use Modules\Products\Models\Product;
 use Modules\Products\Transformers\ProductResource;
 
@@ -13,11 +18,30 @@ class ProductsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
         $products = Product::with(['category', 'brand'])->active()->paginate();
 
-        return api()->paginate($products, ProductResource::class);
+        $response["records"] = ProductResource::collection($products);
+        $response["paginationInfo"] = $this->getPaginationInfo($products);
+
+        if ($request->has("forCategory")) {
+            $response["category"] = new CategoryResource(
+                Category::active()
+                    ->where("slug", $request->string("forCategory")->value())
+                    ->firstOrFail()
+            );
+        }
+
+        if ($request->has("forBrand")) {
+            $response["brand"] = new BrandResource(
+                Brand::active()
+                    ->where("slug", $request->string("forBrand")->value())
+                    ->firstOrFail()
+            );
+        }
+
+        return api()->success($response);
     }
 
     /**
@@ -40,5 +64,21 @@ class ProductsController extends Controller
             "record" => $record,
             "relatedProducts" => $relatedProducts,
         ]);
+    }
+
+    /**
+     * get pagination info
+     */
+    private function getPaginationInfo(LengthAwarePaginator $products): object
+    {
+        return (object) [
+            "current_page" => $products->currentPage(),
+            "per_page" => $products->perPage(),
+            "total" => $products->total(),
+            "last_page" => $products->lastPage(),
+            "from" => $products->firstItem(),
+            "to" => $products->lastItem(),
+            "has_more_pages" => $products->hasMorePages(),
+        ];
     }
 }
